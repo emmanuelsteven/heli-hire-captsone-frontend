5055525b9791141aedd19aed6481c23146f09220
emmanuelsteven <write2stevemmy@gmail.com>
2023-10-02 12:43:56 +0100

    update setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className= "bg-black">
-      <h1 className= 'text-white'>practice tailwind</h1>
+    <div className="bg-black">
+      <h1 className="text-white">practice tailwind</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
