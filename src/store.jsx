a29b0a893341d07d898ca081152d8075b26b31f6
Bisaso Ronald <bronnie313@gmail.com>
2023-10-04 08:03:49 +0300

    add pagination slice action and reducer to the homepage

--- src/store.jsx
+++ src/store.jsx
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helicopterReducer from './features/helicopters/helicopterSlice';
+import paginationReducer from "./features/pagination/paginationSlice";
+
 const store = configureStore({
     reducer: {
         helicopter: helicopterReducer,
+        pagination: paginationReducer,
     },
 })
 
