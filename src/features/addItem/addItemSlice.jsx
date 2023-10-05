a64ecc42b92d2000a40731ab30aca915580346c8
Bisaso Ronald <bronnie313@gmail.com>
2023-10-05 22:01:52 +0300

    add additemSlice and add Item component

---
+++ src/features/addItem/addItemSlice.jsx
@@ -0,0 +1,22 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const  url = 'http://localhost:3000/api/helicopter'
+
+const addItem = createAsyncThunk('item/addItem', async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+});
+
+const initialState = {
+    
+}
+const addItemSlice = createSlice({
+    name: 'item',
+    initialState,
+    reducers: {},
+    extraReducers,
+})
+
+export { addItem };
+export default addItemSlice.reducer;
--- src/store.jsx
+++ src/store.jsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helicopterReducer from './features/helicopters/helicopterSlice';
 import paginationReducer from "./features/pagination/paginationSlice";
+import addItemReducer from "./features/addItem/addItemSlice";
 
 const store = configureStore({
     reducer: {
         helicopter: helicopterReducer,
         pagination: paginationReducer,
+        item: addItemReducer,
     },
 })
 
