cd616a1b8fed874ca0290422516a7fd02a0d5359
Bisaso Ronald <bronnie313@gmail.com>
2023-10-06 01:29:36 +0300

    add styling to the home page

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -34,74 +34,76 @@ const AddItem = () => {
     };
 
     return (
-        <section>
-            <form onSubmit={handleFormSubmission}>
-                <h2>Add New Helicopter</h2>
-                <div>     
-                    <input
-                    placeholder="Helicopter name"
-                    type='text'
-                    name='name'
-                    value={formData.name}
-                    onChange={handleFormInput}
-                    />
-                </div>
-                <div>
-                    <input
-                    placeholder="contact email"
-                    type="text"
-                    name="contact"
-                    value={formData.contact}
-                    onChange={handleFormInput}
-                    />
-                </div>
-                <div>
-                    <input 
-                    placeholder="price/hr"
-                    type="number"
-                    name="price" 
-                    value={formData.price}
-                    onChange={handleFormInput}
-                     />
-                </div>
-                <div>
-                    <input 
-                    placeholder="carriage capacity"
-                    type="number"
-                    name="carriage_capacity"
-                    value={formData.carriage_capacity}
-                    onChange={handleFormInput}
-                     />
-                </div>
-                <div>
-                    <input 
-                    placeholder="image url"
-                    type="text"
-                    name="image"
-                    value={formData.image}
-                    onChange={handleFormInput}
-                    />
-                </div>
-                <div>
-                    <input 
-                    placeholder="model number"
-                    type="number"
-                    name="model"
-                    value={formData.model}
-                    onChange={handleFormInput}
-                     />
-                </div>
-                <div>
-                    <input 
-                    placeholder="description "
-                    type="textarea"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleFormInput}
-                     />
-                </div>
-                <button type='submit'>submit form</button>
-            </form>
+        <section className="item-page">
+            <div className="background-cover">
+                <form className="form-page" onSubmit={handleFormSubmission}>
+                    <h2 className="item-form">Add New Helicopter</h2>
+                    <div>     
+                        <input
+                        placeholder="Helicopter name"
+                        type='text'
+                        name='name'
+                        value={formData.name}
+                        onChange={handleFormInput}
+                        />
+                    </div>
+                    <div>
+                        <input
+                        placeholder="contact email"
+                        type="text"
+                        name="contact"
+                        value={formData.contact}
+                        onChange={handleFormInput}
+                        />
+                    </div>
+                    <div>
+                        <input 
+                        placeholder="price/hr"
+                        type="number"
+                        name="price" 
+                        value={formData.price}
+                        onChange={handleFormInput}
+                        />
+                    </div>
+                    <div>
+                        <input 
+                        placeholder="carriage capacity"
+                        type="number"
+                        name="carriage_capacity"
+                        value={formData.carriage_capacity}
+                        onChange={handleFormInput}
+                        />
+                    </div>
+                    <div>
+                        <input 
+                        placeholder="image url"
+                        type="text"
+                        name="image"
+                        value={formData.image}
+                        onChange={handleFormInput}
+                        />
+                    </div>
+                    <div>
+                        <input 
+                        placeholder="model number"
+                        type="number"
+                        name="model"
+                        value={formData.model}
+                        onChange={handleFormInput}
+                        />
+                    </div>
+                    <div>
+                        <input 
+                        placeholder="description "
+                        type="textarea"
+                        name="description"
+                        value={formData.description}
+                        onChange={handleFormInput}
+                        />
+                    </div>
+                    <button className="submit-btn" type='submit'>submit form</button>
+                </form>
+            </div>
       </section>
     )
 }
