81288245031f9cd8db854daae8f3ae11b6c9eb3e
Bisaso Ronald <bronnie313@gmail.com>
2023-10-06 16:20:25 +0300

    configure store and add sessions to the store

--- src/store.jsx
+++ src/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helicopterReducer from './features/helicopters/helicopterSlice';
 import paginationReducer from "./features/pagination/paginationSlice";
+import sessionsReducer from "./features/sessions/sessionsSlice";
 import signupReducer from "./features/signup/signupSlice";
 import loginReducer from "./features/Login/loginSlice";
 
@@ -8,6 +9,7 @@ const store = configureStore({
     reducer: {
         helicopter: helicopterReducer,
         pagination: paginationReducer,
+        sessions: sessionsReducer,
         signup: signupReducer,
         login: loginReducer,
     },
