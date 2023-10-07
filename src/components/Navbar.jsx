60555aa518f8c992c66e75bd589cbc625498b0f6
Ismail Ali <ismailchangezi801@gmail.com>
2023-10-07 12:17:21 +0500

    Make some changes to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,17 +12,18 @@ function Navbar() {
 
   return (
     <>
-      <IconContext.Provider value={{ color: "#fff" }}>
+      <IconContext.Provider value={{ color: "#000" }}>
         <div className="navbar">
           <Link to="#" className="menu-bars">
             <FaIcons.FaBars onClick={showSidebar} />
           </Link>
         </div>
+
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
           <ul className="nav-menu-items" onClick={showSidebar}>
             <li className="navbar-toggle">
               <Link to="#" className="menu-bars">
-                <AiIcons.AiOutlineClose />
+                <AiIcons.AiOutlineClose className="close-bar" />
               </Link>
             </li>
             {SidebarData.map((item, index) => {
